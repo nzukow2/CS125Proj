b9a9f03d6c4cf9a5b54a40a4a0ac3048315bad9a
Nathaniel Zukowski <nzukow2@illinois.edu>
2019-11-22 07:18:14 -0600

    work done on SecretFunction

--- app/src/main/java/com/example/finalprojectcs125/MainActivity.java
+++ app/src/main/java/com/example/finalprojectcs125/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static int points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +16,136 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Do this and then do that
     }
+
+    public int secretNameMethod() {
+        //Set on click listener here
+    String userNameInput = "set me up boss";
+        // Test cases to deal with if empty string, nan, null, etc.
+        if (userNameInput.equals("Geoff Challen")) { // Prof
+            points = 1000000000; // One billion points
+            // Display message that says thanks for the great semester :)
+        }
+        if (userNameInput.equals("Ben Nordick")) { // The man
+            points = 1000000000;
+            // Display message that thanks him for his work
+        }
+        if (userNameInput.equals("Heather Huynh")) { // EMP
+            points = 1000000000;
+            // Display message that thanks her for her work! (EMP, TA)
+        }
+        if (userNameInput.equals("Aabhas Chauhan")) {
+            points = 1000000000;
+            // Display message that thanks him for his work
+        }
+        if (userNameInput.equals("Jishnu Dey")) {
+            points = 1000000000;
+            // Display message that thanks him for his work
+        }
+        if (userNameInput.equals("Mingkun Gao")) {
+            points = 1000000000;
+            // Display message that thanks him for his work
+        }
+        if (userNameInput.equals("Mohammed Hassan")) {
+            points = 1000000000;
+            // Display message that thanks him for his work
+        }
+        if (userNameInput.equals("Nerla Jean-Louis")) {
+            points = 1000000000;
+        }
+        if (userNameInput.equals("Silas Hsu")) {
+            points = 1000000000;
+            // Display message that thanks him for his work
+        }
+        if (userNameInput.equals("Vinith Krishnan")) {
+            points = 1000000000;
+            // Display message that thanks him for his work
+        }
+        String[] caArray = new String[79];
+        caArray[0] = "Alan Andrade";
+        caArray[1] = "";
+        caArray[2] = "";
+        caArray[3] = "";
+        caArray[4] = "";
+        caArray[5] = "";
+        caArray[6] = "";
+        caArray[7] = "";
+        caArray[8] = "";
+        caArray[9] = "";
+        caArray[10] = "";
+        caArray[11] = "";
+        caArray[12] = "";
+        caArray[13] = "";
+        caArray[14] = "";
+        caArray[15] = "";
+        caArray[16] = "";
+        caArray[17] = "";
+        caArray[18] = "";
+        caArray[19] = "";
+        caArray[20] = "";
+        caArray[21] = "";
+        caArray[22] = "";
+        caArray[23] = "";
+        caArray[24] = "";
+        caArray[25] = "";
+        caArray[26] = "";
+        caArray[27] = "";
+        caArray[28] = "";
+        caArray[29] = "";
+        caArray[30] = "";
+        caArray[31] = "";
+        caArray[32] = "";
+        caArray[33] = "";
+        caArray[34] = "";
+        caArray[35] = "";
+        caArray[36] = "";
+        caArray[37] = "";
+        caArray[38] = "";
+        caArray[39] = "";
+        caArray[40] = "";
+        caArray[41] = "";
+        caArray[42] = "";
+        caArray[43] = "";
+        caArray[44] = "";
+        caArray[45] = "";
+        caArray[46] = "";
+        caArray[47] = "";
+        caArray[48] = "";
+        caArray[49] = "";
+        caArray[50] = "";
+        caArray[51] = "";
+        caArray[52] = "";
+        caArray[53] = "";
+        caArray[54] = "";
+        caArray[55] = "";
+        caArray[56] = "";
+        caArray[57] = "";
+        caArray[58] = "";
+        caArray[59] = "";
+        caArray[60] = "";
+        caArray[61] = "";
+        caArray[62] = "";
+        caArray[63] = "";
+        caArray[64] = "";
+        caArray[65] = "";
+        caArray[66] = "";
+        caArray[67] = "";
+        caArray[68] = "";
+        caArray[69] = "";
+        caArray[70] = "";
+        caArray[71] = "";
+        caArray[72] = "";
+        caArray[73] = "";
+        caArray[74] = "";
+        caArray[75] = "";
+        caArray[76] = "";
+        caArray[77] = "";
+        caArray[78] = "";
+        caArray[79] = "";
+        
+        for (int i = 0; i < caArray.length; i++) {
+
+        }
+        return points;
+    }
 }
+
