c98ab9eeadf09211782ef4147d3d0755eeb9e677
Nathaniel Zukowski <nzukow2@illinois.edu>
2019-12-06 12:15:36 -0600

    push - groupDebug sesh

--- app/src/main/java/com/example/finalprojectcs125/MainActivity.java
+++ app/src/main/java/com/example/finalprojectcs125/MainActivity.java
@@ -125,6 +125,8 @@ public class MainActivity extends AppCompatActivity {
             settingsMenuTransition();
         });
 
+        TextView menuTextPoints = findViewById(R.id.menuPointsTextView);
+        menuTextPoints.setText("You have: " + points + " points");
 
     }
 
@@ -147,7 +149,6 @@ public class MainActivity extends AppCompatActivity {
      * code that allows the clicker to work properly.
      */
     public void clickerGame() {
-        setContentView(R.layout.clickergame_gaming);
         /**
          * this textView is the timer.
          */
@@ -324,6 +325,9 @@ public class MainActivity extends AppCompatActivity {
         clickerGameButton.setOnClickListener(iw -> {
             clickerGameTransition();
         });
+
+        TextView menuTextPoints = findViewById(R.id.menuPointsTextView);
+        menuTextPoints.setText("You have: " + points + " points");
     }
 
 
@@ -357,14 +361,17 @@ public class MainActivity extends AppCompatActivity {
         Button upgradePointsButton = findViewById(R.id.gotoUpgradesFromClickerAppButton);
         upgradePointsButton.setOnClickListener(transitionTime -> {
             clickerUpgradePageOneTransition();
-            // TODO: 2019-12-05 Add transition to upgrade menu!
         });
 
+
+
+        //clickerGame();
+        /*
         ImageButton clickerio = findViewById(R.id.clicker);
         clickerio.setOnClickListener(yoyo -> {
             clickerGame();
         });
-
+         */
 
     }
 
