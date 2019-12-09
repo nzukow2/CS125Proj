96410a0bc2370a2f62d4b2a8777e593f7ff42ad6
RevanReborn2000 <54616107+RevanReborn2000@users.noreply.github.com>
2019-12-09 00:31:36 -0600

    Merge branch 'master' of https://github.com/nzukow2/CS125Proj
    # Conflicts:
    #	app/src/main/java/com/example/finalprojectcs125/MainActivity.java

--- app/src/main/java/com/example/finalprojectcs125/MainActivity.java
+++ app/src/main/java/com/example/finalprojectcs125/MainActivity.java
@@ -326,6 +326,7 @@ public class MainActivity extends AppCompatActivity {
         time = new CountDownTimer(30000,1000) {
             @Override
             public void onTick(long millisUntilFinished) {
+                empezario.setVisibility(View.INVISIBLE);
                 tiempo--;
                 timerio.setText("Time: " + tiempo);
                 //finished.setText("");
@@ -337,6 +338,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 TextView clickerapppTs = findViewById(R.id.clickerAppPointsTextView);
+                empezario.setVisibility(View.VISIBLE);
                 clickerapppTs.setTextColor(Color.BLACK);
                 clickerapppTs.setBackgroundColor(Color.RED);
                 empezario.setEnabled(true);
