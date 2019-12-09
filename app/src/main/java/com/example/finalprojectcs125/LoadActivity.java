745df293121c407336a30234685cc71981fc5e72
Nathaniel Zukowski <nzukow2@illinois.edu>
2019-12-08 20:47:53 -0600

    Changed the initial activity to loadActivity & main a "loadscreen"

--- app/src/main/java/com/example/finalprojectcs125/LoadActivity.java
+++ app/src/main/java/com/example/finalprojectcs125/LoadActivity.java
@@ -2,7 +2,9 @@ package com.example.finalprojectcs125;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LoadActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class LoadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(LoadActivity.this, MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        },4000);
     }
 }
